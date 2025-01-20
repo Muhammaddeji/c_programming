5d15bee938b409083072294d53b0837c0acabae5
Muhammaddeji <adegbitesheriff300@gmail.com>
2025-01-20 19:55:21 +0100

    Hello World

---
+++ susbcribe.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	x = 10;
+
+	//Expression - Anything that can be evaluated (to 1 value)
+	//statement
+	//declaration
+	//initialization
+	
+	int y = x / 2;
+
+	printf("Hello World!\n");
+	return 0;
+}
+
+//space insensitive language.
